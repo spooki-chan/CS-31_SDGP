8b2059506ca9a1e1dbe3a9cd2dc3ceb2fe16cd03
FrancoDeSilva <vinod.20200746@iit.ac.lk>
2022-04-21 09:44:57 +0530

    Changes

--- website/static/jQueryA.js
+++ website/static/jQueryA.js
@@ -1,48 +1,52 @@
 $(function() {
-	  $("#style").selectmenu();
+	$("#style").selectmenu();
 });
 
 $(function() {
-	$("#type").selectmenu();
+  $("#type").selectmenu();
 });
 
 
 
 $(function() {
-	$("#slider-range").slider({
-		range:true,
-		min: 0,
-		max: 350,
-		values: [ 75, 250 ],
-		slide: function( event, ui ){
-			$("#amount").val(  ui.values[ 0 ] + "-" + ui.values[ 1 ] );
-		}
-	});
-	
-	$("#amount").val( $(" #slider-range").slider( "values", 0 ) + " - " + $("#slider-range").slider( "values", 1 ) );
+  $("#slider-range").slider({
+	  range:true,
+	  min: 0,
+	  max: 600,
+	  values: [ 75, 350 ],
+	  slide: function( event, ui ){
+		  $("#amount").val(  ui.values[ 0 ] + "-" + ui.values[ 1 ] );
+	  }
+  });
+  
+  $("#amount").val( $(" #slider-range").slider( "values", 0 ) + " - " + $("#slider-range").slider( "values", 1 ) );
 });
 
 
 $(function() {
-	$( "#Search" ).on("click", function(){
-		
-		var recstyle = $("#style").val();
-		var rectype = $("#type").val();
-		
-		var output="<ul>";
-		   for (var i in data.recipes) {
-			   if (( recstyle == data.recipes[i].style) || (recstyle=="Any"))
-			   if (( rectype == data.recipes[i].type) || (rectype=="Any"))
-			
+  $( "#Search" ).on("click", function(){
+	  
+	  var recstyle = $("#style").val();
+	  var rectype = $("#type").val();
+	  var mincal = $("#slider-range").slider("option", "values")[0];
+	  var maxcal = $("#slider-range").slider("option", "values")[1];
+	  
+	  var output="<ul>";
+		 for (var i in data.recipes) {
+			 if (( recstyle == data.recipes[i].style) || (recstyle=="Any"))
+			 if (( rectype == data.recipes[i].type) || (rectype=="Any"))
+			 if (( data.recipes[i].calorie >= mincal && data.recipes[i].calorie <= maxcal ))
 			 
-			  
-			   {
-				   {
-					   {
-						   {
-							   output+="<h2><li>" + data.recipes[i].name +"</li></h2>" + "<img src=" + data.recipes[i].picture + ">"+"<p>"+ data.recipes[i].description + "</p>" + "<button><a href='" + data.recipes[i].url + "'>Visit Page</a></button><hr>";
-						   } } } } }
-				output+="</ul>";
-				document.getElementById( "Placeholder" ).innerHTML = output;
-		   });
-	});
+		  
+		   
+			
+			 {
+				 {
+					 {
+						 {
+							 output+="<h2><li>" + data.recipes[i].name +"</li></h2>" + "<img src=" + data.recipes[i].picture + ">"+"<p>"+ data.recipes[i].calorie + "</p>" + "<button><a href='" + data.recipes[i].url + "'>Visit Page</a></button><hr>";
+						 } } } } }
+			  output+="</ul>";
+			  document.getElementById( "Placeholder" ).innerHTML = output;
+		 });
+  });
