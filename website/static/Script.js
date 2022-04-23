fe6cc2b9d9f6887bd213c501499be6b6ed19a1d2
FrancoDeSilva <vinod.20200746@iit.ac.lk>
2022-04-23 14:34:14 +0530

    ajax

--- website/static/Script.js
+++ website/static/Script.js
@@ -7,6 +7,8 @@
             $(this).attr('disabled', true);
             var recipeID = $(this).closest("p").attr("id");
             var recipeName = $(this).closest("p").attr("class");
+            console.log(recipeID);
+            console.log(recipeName);
 
         });
     });
